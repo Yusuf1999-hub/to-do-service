abb8deee0d8750a9fc64f50054c2c15aef6cd882
Yusuf1999-hub <yusufkhusanovic@gmail.com>
2021-12-22 19:13:48 +0500

    from serial into uuid

--- migrations/000001_create_tasks_table.up.sql
+++ migrations/000001_create_tasks_table.up.sql
@@ -1,11 +1,14 @@
 CREATE TABLE IF NOT EXISTS tasks(
-    id SERIAL PRIMARY KEY,
+    id UUID NOT NULL,
     assignee VARCHAR(20) NOT NULL,
     title TEXT NOT NULL,
     summary TEXT NOT NULL,
-    deadline DATE DEFAULT CURRENT_DATE,
+    deadline timestamp DEFAULT CURRENT_DATE,
     status TEXT DEFAULT 'active',
-    created_at timestamp,
-    updated_at timestamp,
+    created_at timestamp DEFAULT CURRENT_DATE,
+    updated_at timestamp DEFAULT CURRENT_DATE,
     deleted_at timestamp
 );
+
+
+UPDATE tasks SET new_id = CAST(LPAD(TO_HEX(id), 32, '0') AS UUID);
