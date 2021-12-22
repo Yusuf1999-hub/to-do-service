a3225de1ba1f811856298e2e844f60e17b8224c1
Yusuf1999-hub <yusufkhusanovic@gmail.com>
2021-12-22 19:21:53 +0500

    can not remember

--- storage/repo/task.go
+++ storage/repo/task.go
@@ -2,14 +2,15 @@ package repo
 
 import (
 	pb "github.com/Yusuf1999-hub/to-do-service/genproto"
+	"time"
 )
 
 // TaskStorageI ...
 type TaskStorageI interface {
 	Create(pb.Task) (pb.Task, error)
-	Get(id int64) (pb.Task, error)
+	Get(id string) (pb.Task, error)
 	List(page, limit int64) ([]*pb.Task, int64, error)
 	Update(pb.Task) (pb.Task, error)
-	Delete(id int64) error
-	ListOverdue(page, limit int64, time string) ([]*pb.Task, int64, error)
+	Delete(id string) error
+	ListOverdue(page, limit int64, time time.Time) ([]*pb.Task, int64, error)
 }
